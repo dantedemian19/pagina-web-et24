4dcdfec1c75d9e13268ac2f41f433e7fadbdf867
dantedemian19 <dantedemian19@gmail.com>
2021-11-12 14:34:38 -0300

    puto el que lee

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const addLocalStrategy = require('./localPassport-config') // estrategia de aute
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 
 // conecta con la base de datos
 mongoose.connect('mongodb://localhost/ET24', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
